6d204bcf6d49b94a52bb0e1bdc422d881813141b
Roman <roman.chaban1001@gmail.com>
2025-03-06 02:22:12 +0200

    Remover titles and set only default title.

--- src/app/all-recipes/page.tsx
+++ src/app/all-recipes/page.tsx
@@ -4,10 +4,7 @@ import { Recipes } from "@/features/Recipes/ui/Recipes";
 import { client } from "@/shared/client/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 
-import { useDocumentTitle } from "@/shared/hooks/useDocumentTitle";
-
 export default function AllRecipesPage() {
-  useDocumentTitle("Next Recipes - All Recipes");
   return (
     <QueryClientProvider client={client}>
       <h1 className='text-4xl font-bold text-center text-blue-950-600 my-4'>
--- src/app/favorite-recipes/page.tsx
+++ src/app/favorite-recipes/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import FavoriteRecipes from "@/features/FavoriteRecipes/ui/FavoriteRecipes";
-import { useDocumentTitle } from "@/shared/hooks/useDocumentTitle";
 
 export default function FavoriteRecipesPage() {
-  useDocumentTitle("Next Recipes - Favorite Recipes");
-
   return <FavoriteRecipes />;
 }
